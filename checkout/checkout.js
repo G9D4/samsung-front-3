d69b69421ad071423828c3c0f34e5533fd0e61a3
Gabriela Díaz <gldt2494@gmail.com>
2024-12-16 21:41:02 -0500

    feat: renderiza items y total en el aside

--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,2 +1,35 @@
-import { getCartGuest } from "../helpers/general";
+import { getCartGuest } from "../helpers/general.js";
+import data from '../data.json' with { type: "json" };
+
+const cartItems = getCartGuest();
+
+const renderItemsSection = () => {
+    const cartContainer = document.getElementById("cart-items-section");
+    const cartSubtotal = document.getElementById("cart-subtotal");
+    const cartTotal = document.getElementById("cart-total");
+    let renderedItems = '';
+    let priceSum = null;
+    
+    cartItems.forEach(element => {
+        let productInfo = data.productos.filter(p => p.id == element.id)[0];
+        priceSum = priceSum + productInfo.precio;
+        renderedItems +=
+        `<article class="checkout-detail_product">
+            <div class="checkout-detail_product_image">
+                <img src=${productInfo.url[0]} alt=${productInfo.id}>
+                <span>${element.quantity}</span>
+            </div>
+            <p>${productInfo.nombre}</p>
+            <p>S/. ${productInfo.precio.toFixed(2)}</p>
+        </article>`
+    });
+
+    cartContainer.innerHTML = renderedItems;
+    cartSubtotal.innerHTML = `S/. ${priceSum.toFixed(2)}`;
+    cartTotal.innerHTML = `S/. ${(13 + priceSum).toFixed(2)}`;
+};
+
+
+
+renderItemsSection();
 
